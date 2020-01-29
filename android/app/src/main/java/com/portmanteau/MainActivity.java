374733a0c4b00e50492f810eb73c154cd281625e
George Albertson <gdea73@gmail.com>
2020-01-28 22:32:12 -0800

    burned it all down

--- android/app/src/main/java/com/portmanteau/MainActivity.java
+++ android/app/src/main/java/com/portmanteau/MainActivity.java
@@ -1,24 +1,15 @@
 package com.portmanteau;
 
 import com.facebook.react.ReactActivity;
-import com.google.android.gms.ads.MobileAds;
-import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
-	private static final String ADMOB_APP_ID
-		= "ca-app-pub-8559716447664382~6704873685";
 
-	protected void onCreate(Bundle savedInstancestate) {
-		super.onCreate(savedInstancestate);
-		MobileAds.initialize(this, ADMOB_APP_ID);
-	}
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "Portmanteau";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Portmanteau";
+  }
 }
